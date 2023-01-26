e3926b62f9f35fef00d567839f9a7e5271257db8
Okoye Philip <okoyephlp07322@gmail.com>
2023-01-26 02:11:04 -0800

    updated

---
+++ 003-libraries/15-say.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+from sayins import hello
+import sys
+if len(sys.argv) == 2:
+    hello(sys.argv[1])
---
+++ 003-libraries/sayins.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def main():
+    hello("David")
+    goodbye("Muna")
+
+def hello(name):
+    print(f"hello, {name}")
+
+
+def goodbye(name):
+    print(f"goodbye, {name}")
+
+
+if __name__ == "__main__":
+    main()
