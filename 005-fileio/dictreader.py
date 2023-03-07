1edb07648d510c12815ea7ea737c85f70300c20d
Okoye Philip <okoyephlp07322@gmail.com>
2023-03-07 00:43:31 -0800

    update dictreader

---
+++ 005-fileio/dictreader.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import csv
+
+students = []
+with open("dictreader.csv") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        students.append({"name":row["name"], "home":row["home"]})
+
+for student in sorted(students, key=lambda student: student["name"]):
+    print(f"{student['name']} is from {student['home']}")
+
