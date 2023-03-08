f4162608427a3462999c578dd9a580a7a9f9ed0e
Okoye Philip <okoyephlp07322@gmail.com>
2023-03-08 02:59:46 -0800

    add basics

---
+++ 006-OOP/1-student.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+name = input("Name ")
+house = input("House ")
+print(f"{name} from {house}")
---
+++ 006-OOP/5-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def main():
+    student = get_student()
+    if student[0] == "Padma":
+        student[1] = "Okolo"
+    print(f"{student[0]} from {student[1]}")
+
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return [name, house]
+
+
+if __name__ == "__main__":
+    main()
